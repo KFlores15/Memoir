8c23446bcf6d8e9ef9e5066273ef524c783a0b6b
Kevin Flores <kflores2@ucsc.edu>
2018-02-18 23:30:52 -0800

    Most recent Build merged

---
+++ Memoir/Assets/Scripts/Puzzle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzle : MonoBehaviour {
+
+	public static string correctCode="397";
+	public static string playerCode = "";
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (playerCode);
+
+	}
+
+	void OnMouseUp()
+	{
+		playerCode += gameObject.name;
+		totalDigits += 1;
+	}
+	
+}
---
+++ Memoir/Assets/Scripts/TEmp/Monster.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Monster : MonoBehaviour
+{
+    public Renderer rend;
+    public GameObject player;
+    public Vector2 spawn;
+    public int load;
+    public bool face_right;
+
+    private bool follow = true;
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        rend.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Behaviour(follow);
+    }
+
+    
+
+    private void Behaviour(bool follow)
+    {
+            transform.Translate(Vector2.right * (4/2) * Time.deltaTime / 2);
+            Debug.Log("Attempting to follow player");
+    }
+    
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+		{
+			Debug.Log("Colided with player");
+			StartCoroutine(Wait());
+}
+	}
+
+	//waits for fade and then destroys monster and 'kills' the friend
+	IEnumerator Wait()
+	{
+		yield return new WaitForSecondsRealtime(0.5f);
+		Destroy(gameObject);
+        SceneManager.LoadScene(load);
+		player.transform.position = spawn;
+
+        player.transform.localScale = new Vector3(face_right ? 1 : -1 , 1, 1);
+    }
+}
