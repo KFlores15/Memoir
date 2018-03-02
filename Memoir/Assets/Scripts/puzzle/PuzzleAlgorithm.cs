5f3ccf77f2fb79d61e2e47142d75bd6bd12a94ce
Mayank Tiwari <mayank@rockinfamily.com>
2018-03-02 10:55:50 -0800

    Puzzle Algorithm works and outputs to 4 images, changing their colors based on if input is higher or lower than solution for each individual digit. TODO: puzzleOnFinish event handling

--- Memoir/Assets/Scripts/puzzle/PuzzleAlgorithm.cs
+++ Memoir/Assets/Scripts/puzzle/PuzzleAlgorithm.cs
@@ -7,52 +7,43 @@ using UnityEngine.Events;
 public class PuzzleAlgorithm : MonoBehaviour {
     int sol;
     public InputField input;
-	public Image int1, int2, int3, int4;
 	public Text debug;
-    string[] feedback;
     const float SOL_MIN = 1000f;
     const float SOL_MAX = 9999f;
-	Image[] images;
-	private UnityAction submit;
+	public Image[] images;
 
-    // Use this for initialization
     void Start () {
-		feedback = new string[4];
-        input = gameObject.GetComponent<InputField>();
-		var e = new InputField.SubmitEvent();
-		e.AddListener (PuzzleSubmit);
-		submit = PuzzleSubmit;
-		input.onEndEdit.AddListener (submit);
-        sol = (int) Mathf.Round(Random.Range(SOL_MIN, SOL_MAX));
-		debug = GetComponent<Text> ();
-		for (int i = 0; i < 4; i++) {
-		}
-
-        //initializes the solution, a random number between
+    	sol = (int) Mathf.Round(Random.Range(SOL_MIN, SOL_MAX));
+	    //initializes the solution, a random number between
         //1000-9999
     }
-	// Update is called once per frame
-	void Update () {
-		//enter is pressed 
-	}
-	void PuzzleSubmit(string guess){
+	public void PuzzleSubmit(){
 		int G = 0;
 		int tempsol = sol;
 		string result = "";
-		G = int.Parse (guess);
-		for(int i = 4; i > 0; i--) {
-			int Isol = sol % 10;
+		G = int.Parse (input.text);
+		for(int i = 3; i >= 0 ; i--) {
+			int Isol = tempsol % 10;
 			int Iguess = G % 10;
 			if(Isol == Iguess){
-				result = result + "= ";
+				images [i].color = Color.green;
+				result = "= " + result;
 			}else if(Isol > Iguess){
-				result = result + "> ";
+				images [i].color = Color.yellow;
+				result = "< " + result;
 			}else{
-				result = result + "< ";
+				images [i].color = Color.blue;
+				result = "> " + result;
 			}
+			tempsol = tempsol / 10;
+			G = G / 10;
 		}
-		debug.text = result;
+		/* DEBUG CODE
+		if (result.Length > 1) {
+			debug.text = result.Trim ();
+		} else {
+			debug.text = "ERROR: result was not calculated";
+		}*/
 		input.ActivateInputField ();
-	}
-    
+	}   
 }
