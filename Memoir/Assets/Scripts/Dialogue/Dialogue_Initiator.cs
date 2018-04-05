bf406a8943d24e87588e9f5856545ce183ed90df
Matt Loyola <mjloyola@ucsc.edu>
2018-04-04 21:30:19 -0700

    Intro Scene added with dialogue

---
+++ Memoir/Assets/Scripts/Dialogue/Dialogue_Initiator.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialogue_Initiator : MonoBehaviour {
+	void Start() {
+		GetComponent<DialogueTrigger>().TriggerDialogue();
+	}
+}
--- Memoir/Assets/Scripts/SpaceCutscne1.cs
+++ Memoir/Assets/Scripts/SpaceCutscne1.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class SpaceCutscne1 : MonoBehaviour
 {
+	void Start() {
+		GetComponent<DialogueTrigger>().TriggerDialogue();
+
+	}
+
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
